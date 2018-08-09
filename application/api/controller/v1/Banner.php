03caed32ea4c17db59d06c81d59e13eb09674313
acwhale <csx5800@163.com>
2018-08-09 18:04:18 +0800

    开始试水

---
+++ application/api/controller/v1/Banner.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hello
+ * Date: 2018/8/9
+ * Time: 17:35
+ */
+
+namespace app\api\controller\v1;
+
+
+class Banner {
+
+    /**
+     * @param $id
+     * @url: banner/:id
+     * @method:get
+     */
+    public function getBanner($id){
+
+    }
+}
---
+++ public/phpinfo.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hello
+ * Date: 2018/8/9
+ * Time: 11:09
+ */
+
+phpinfo();
